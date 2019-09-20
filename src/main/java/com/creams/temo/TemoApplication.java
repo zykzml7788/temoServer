581c38bf6c8ca15830aac252cdadc1e539631363
zhangyingkai <ykzhang@creams.io>
2019-09-20 16:54:44 +0800

    提交代码

--- src/main/java/com/creams/temo/TemoApplication.java
+++ src/main/java/com/creams/temo/TemoApplication.java
@@ -1,14 +1,9 @@
 package com.creams.temo;
 
-import com.github.pagehelper.PageHelper;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.omg.PortableInterceptor.Interceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.util.Properties;
 
 @SpringBootApplication
 @MapperScan("com.creams.temo.mapper")
--- src/test/java/com/creams/temo/TemoApplicationTests.java
+++ src/test/java/com/creams/temo/TemoApplicationTests.java
@@ -13,9 +13,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
