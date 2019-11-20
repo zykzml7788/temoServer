3b2da223e75b9b2d49a39f822caea510aa26cc8b
jianglangping <jianglangping@zbycorp.com>
2019-11-20 11:29:52 +0800

    redis配置-新增redis工具类

---
+++ src/main/java/com/creams/temo/config/Status.java
@@ -0,0 +1,41 @@
+package com.creams.temo.config;
+
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 状态枚举
+ * @Author: Jelly
+ *
+ */
+
+public abstract class Status {
+    /**
+     * 过期时间相关枚举
+     */
+    public static enum ExpireEnum{
+        //未读消息的有效期为30天
+        UNREAD_MSG(30L, TimeUnit.DAYS);
+        /**
+         * 过期时间
+         */
+        private Long time;
+        /**
+         * 时间单位
+         */
+        private TimeUnit timeUnit;
+
+        ExpireEnum(Long time, TimeUnit timeUnit) {
+            this.time = time;
+            this.timeUnit = timeUnit;
+        }
+
+        public Long getTime() {
+            return time;
+        }
+
+        public TimeUnit getTimeUnit() {
+            return timeUnit;
+        }
+    }
+}
