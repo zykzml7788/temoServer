07b520ccf7eb6323d5cf00795b8dc89bcb477fff
Administrator <zykzml7788@sina.com>
2019-12-11 20:43:09 +0800

    fix:删除无用包

--- src/main/java/com/creams/temo/controller/CheckCenterController.java
+++ src/main/java/com/creams/temo/controller/CheckCenterController.java
@@ -1,6 +1,5 @@
 package com.creams.temo.controller;
 
-import com.creams.temo.config.MyWebSocketHandler;
 import com.creams.temo.service.WebSocketServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
