b851b75667a13a2533c82dd041fdac5b61b9ab3f
Administrator <zykzml7788@sina.com>
2020-03-14 22:49:48 +0800

    添加跨域设置

---
+++ src/main/java/com/creams/temo/config/CronConfig.java
@@ -0,0 +1,22 @@
+package com.creams.temo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class CronConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
